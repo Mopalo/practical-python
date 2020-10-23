1033c4c16a4448ea63c7917e0973c7e009830ec4
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-23 10:13:06 +0200

    Update bounce.py
    Lets see if this works

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounceHeight = 3/5
+bounceBack = height * bounceHeight
+howMany = 1
+
+while howMany < 5:
+    print("Bounce number:", howMany, "Height:", bounceBack)
+    howMany = howMany + 1
