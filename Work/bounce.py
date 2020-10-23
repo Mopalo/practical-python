c5163e58d656e215d651ec26a7e35d62ecc0cf43
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-23 10:24:41 +0200

    Update bounce.py
    Finishing the exercise by giving the number of bounces to 10 and formatting the decimals

--- Work/bounce.py
+++ Work/bounce.py
@@ -5,8 +5,8 @@ height = 100
 howMany = 1
 bounceHeight = 3/5
 
-while howMany < 5:
+while howMany < 11:
     bounceBack = height * bounceHeight
-    print("Bounce number:", howMany, "Height:", bounceBack)
+    print("Bounce number:", howMany, "Height:", round(bounceBack, 4)
     height = bounceBack
     howMany = howMany + 1
