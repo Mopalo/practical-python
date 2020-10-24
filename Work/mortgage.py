302a990a6ff478cb5c932933568ff4fdb2d34e95
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-24 21:51:05 +0200

    Update mortgage.py
    Didnt put the semicolon

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,7 +7,7 @@ payment = 2684.11
 total_paid = 0.0
 month = 1
 
-while month <= 12
+while month <= 12:
     while principal > 0:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
