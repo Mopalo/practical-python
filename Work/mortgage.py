97a5ee56bb5ca41cb4e5be66c662fea15366951e
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-24 22:56:55 +0200

    Update mortgage.py
    fixing things and adding the last else

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -11,7 +11,7 @@ extra_payment_end_month = 108
 extra_payment = 1000
 
 while principal > 0:
-    if month > extra_payment_start_month:
+    if month < extra_payment_start_month:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
         month += 1
@@ -20,5 +20,11 @@ while principal > 0:
     elif month >= extra_payment_start_month & month < extra_payment_end_month:
         principal = principal * (1+rate/12) - (payment + extra_payment)
         total_paid = total_paid + (payment + extra_payment)
+        month += 1
+        print(month, total_paid, principal)
+
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
         month += 1
         print(month, total_paid, principal)
