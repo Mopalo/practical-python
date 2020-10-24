7874a8f0f41e2c932b6d6833e7376dc903d9693e
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-24 22:50:55 +0200

    Update mortgage.py
    Putting it into the loops

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -15,9 +15,10 @@ while principal > 0:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
         month += 1
+        print(month, total_paid, principal)
+
     elif month >= extra_payment_start_month & month < extra_payment_end_month:
         principal = principal * (1+rate/12) - (payment + extra_payment)
         total_paid = total_paid + (payment + extra_payment)
         month += 1
-
-print(month, total_paid, principal)
+        print(month, total_paid, principal)
