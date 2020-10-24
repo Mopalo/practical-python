fad4249dace4aac65b726f3d78b333517a1ca1d3
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-24 21:54:47 +0200

    Update mortgage.py
    Adding the extra coz im a forgetful bastard

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,10 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 month = 1
+extra = 1000
 
 while principal > 0 && month <= 12:
-    principal = principal * (1+rate/12) - payment
+    principal = principal * (1+rate/12) - (payment + extra)
     total_paid = total_paid + payment
     print(month)
     month += 1
