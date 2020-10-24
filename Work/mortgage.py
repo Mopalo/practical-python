5e92e7a73ad08a24c13db6eba38b75f211f5c891
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-24 22:10:13 +0200

    Update mortgage.py
    Trying to see whats happening

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,14 +8,9 @@ total_paid = 0.0
 month = 1
 extra = 1000
 
-while principal > 0 & month <= 12:
+while month <= 12:
     principal = principal * (1+rate/12) - (payment + extra)
     total_paid = total_paid + (payment + extra)
-    month += 1
-
-while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
     month += 1
 
 print('Months taken', month)
