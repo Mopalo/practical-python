5b17fc198c471ffdf7a6aaf96e0366ce0f6e3f0b
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-24 22:41:31 +0200

    Update mortgage.py
    First try at generalizing

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,17 +6,20 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 month = 0
-extra = 1000
-
-while month < 12:
-    principal = principal * (1+rate/12) - (payment + extra)
-    total_paid = total_paid + (payment + extra)
-    month += 1
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
-    month += 1
+    if month > extra_payment_start_month:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+        month += 1
+    elif month >= extra_payment_start_month & month < extra_payment_end_month:
+        principal = principal * (1+rate/12) - (payment + extra_payment)
+        total_paid = total_paid + (payment + extra_payment)
+        month += 1
+
 
 print('Months taken', month)
 print('Total paid', total_paid)
