3ab00b3962209f8645ac40a6d8796d6a734579b8
Kutlo Kenosi <73200950+Mopalo@users.noreply.github.com>
2020-10-24 21:54:04 +0200

    Update mortgage.py
    Alright... as suspected the loop finished before checking... so i did 2 separate loops... one that stops at 12 and one that does the rest

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,11 +7,16 @@ payment = 2684.11
 total_paid = 0.0
 month = 1
 
-while month <= 12:
-    while principal > 0:
-        principal = principal * (1+rate/12) - payment
-        total_paid = total_paid + payment
-        print(month)
-        month += 1
+while principal > 0 && month <= 12:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    print(month)
+    month += 1
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    print(month)
+    month += 1
 
 print('Total paid', total_paid)
